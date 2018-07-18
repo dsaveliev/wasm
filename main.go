26be8dee522de78749a7208e22d10e4b5256be24
Dmitry Saveliev <saveliev.d@gett.com>
2018-07-19 01:38:47 +0300

    Speed up

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	"syscall/js"
+	"unsafe"
+)
+
+const (
+	width  = 320
+	height = 240
+	size   = 307200
 )
 
 var (
-	beforeUnloadCh = make(chan struct{})
-	renderCanvas   = js.Global().Get("renderProcessed")
-	data           = make([]uint8, 1228800, 1228800)
+	beforeUnloadCh   = make(chan struct{})
+	rawData          = js.Value{}
+	data             = [size]uint8{}
+	saveDataCallback = js.Global().Get("saveData")
+	typedArray       = js.Global().Get("Uint8Array")
+	buffer           = js.Memory.Get("buffer")
 )
 
 func beforeUnload(event js.Value) {
@@ -16,36 +26,21 @@ func beforeUnload(event js.Value) {
 }
 
 func process(args []js.Value) {
-	// pixelData := args[0]
 
-	for i := 0; i < 1228800; i += 4 {
-		// pixelData.SetIndex(i, pixelData.Index(i))
-		args[0].SetIndex(i, args[0].Index(i+3).Int()-40)
-		// data[i] = (uint8)(pixelData.Index(i).Int() + 20)
-		// data[i+1] = (uint8)(pixelData.Index(i + 1).Int())
-		// data[i+2] = (uint8)(pixelData.Index(i + 2).Int())
-		// data[i+3] = (uint8)(pixelData.Index(i + 3).Int())
+	rawData = args[0]
+
+	for i := size - 1; i >= 3; i -= 4 {
+		data[i-3] = 255 - rawData.IndexUint8(i-3)
+		data[i-2] = 255 - rawData.IndexUint8(i-2)
+		data[i-1] = 255 - rawData.IndexUint8(i-1)
 	}
 
-	// data := pixelData.Get("data")
-	// length := data.Length()
-	// fmt.Printf("ARGS >>> %#v \n", length)
-	// for i := 0; i < length; i++ {
-	// 	// p := data.Index(i)
-	// 	data.SetIndex(i, 1)
-	// }
-
-	// tData := js.TypedArrayOf(data)
-	// fmt.Println("CONVERT SLICE <<<<<<")
-	// pixelData.Set("data", jsData)
-	// renderCanvas.Invoke(pixelData)
-
-	//renderCanvas.Invoke(pixelData)
+	saveDataCallback.Invoke(typedArray.New(buffer, unsafe.Pointer(&data), size))
 }
 
 func main() {
-	for i := 0; i < 1228800; i += 4 {
-		data[i] = 255
+	for i := 0; i < size; i += 4 {
+		data[i] = 0
 		data[i+1] = 0
 		data[i+2] = 0
 		data[i+3] = 255
@@ -55,12 +50,6 @@ func main() {
 	defer callback.Release()
 	setPrintMessage := js.Global().Get("setDSPCallback")
 	setPrintMessage.Invoke(callback)
-
-	// fillRect := js.Global().Get("fillRect")
-	// fillRect.Invoke(50, 25, 150, 100)
-
-	// drawImage := js.Global().Get("drawImage")
-	// drawImage.Invoke()
 
 	beforeUnloadCb := js.NewEventCallback(0, beforeUnload)
 	defer beforeUnloadCb.Release()
